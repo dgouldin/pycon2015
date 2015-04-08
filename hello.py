59e680076dd9f0795996a500a3a5ad0e2bba51db
David Gouldin <dgouldin@heroku.com>
2015-04-07 19:28:55 -0700

    add logging

--- hello.py
+++ hello.py
@@ -3,6 +3,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
+import logging
+logging.basicConfig(level=logging.DEBUG)
+
 @app.route('/')
 def hello():
+    logging.info("saying hello")
     return 'Hello world!'
