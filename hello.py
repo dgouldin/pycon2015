aba6a3f67975df72586c987ca3baeb512a8359b0
David Gouldin <dgouldin@heroku.com>
2015-04-07 19:37:31 -0700

    add name

--- hello.py
+++ hello.py
@@ -1,12 +1,13 @@
 import os
+import logging
 from flask import Flask
 
 app = Flask(__name__)
 
-import logging
 logging.basicConfig(level=logging.DEBUG)
 
 @app.route('/')
 def hello():
     logging.info("saying hello")
-    return 'Hello world!'
+    name = os.environ.get('NAME', 'World')
+    return 'Hello {}!'.format(name)
