460c5d8de6276139513b9885073743b0eea729c0
David Gouldin <dgouldin@heroku.com>
2015-04-07 23:02:08 -0400

    use redis for name

--- hello.py
+++ hello.py
@@ -1,13 +1,20 @@
 import os
 import logging
+import redis
 from flask import Flask
 
 app = Flask(__name__)
+db = redis.from_url(os.environ['REDISCLOUD_URL'])
 
 logging.basicConfig(level=logging.DEBUG)
 
 @app.route('/')
 def hello():
     logging.info("saying hello")
-    name = os.environ.get('NAME', 'World')
+    name = db.get('name') or 'World'
     return 'Hello {}!'.format(name)
+
+@app.route('/setname/<name>')
+def setname(name):
+    db.set('name', name)
+    return 'Name updated.'
